1ec550be262c50dede93e77c89556ce0a4a392b7
CamrynBowWow <bowdencamryn@gmail.com>
2024-12-07 20:06:10 +0200

    Clerk Auth Setup Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import { Button } from '@/components/ui/button';
+import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-export default function HomePage() {
+export default async function HomePage() {
+	const { userId } = await auth();
+
+	if (userId != null) redirect('/events');
+
 	return (
 		<div className='text-center container my-4 mx-auto'>
 			<h1 className='text-3xl mb-4'>Fancy Home Page</h1>
 			<div className='flex gap-2 justify-center'>
-				<Button>Sign In</Button>
-				<Button>Sign Up</Button>
+				<Button asChild>
+					<SignInButton />
+				</Button>
+				<Button asChild>
+					<SignUpButton />
+				</Button>
+				<UserButton />
 			</div>
 		</div>
 	);
