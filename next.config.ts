6d1e5a3cc0d7e1444a1f6ea766eec8f467652e53
CamrynBowWow <bowdencamryn@gmail.com>
2024-12-19 12:03:49 +0200

    Booking Page Done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	/* next.js 14 issue but not in next.js 15 */
+	// experimental: {
+	// 	staleTimes: {
+	// 		dynamic: 0,
+	// 	},
+	// },
 };
 
 export default nextConfig;
